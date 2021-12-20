11bfc3da1455d45c0872458765f1eec73a1c32e7
Hugo Oliveira <hugo684@gmail.com>
2021-12-20 13:26:28 -0300

    Implemented method to pick colour.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,9 +9,8 @@ defmodule Identicon do
     |> pick_colour()
   end
 
-  def pick_colour(image) do
-    %Identicon.Image{hex: [red, green, blue | _tail]} = image
-    [red, green, blue]
+  def pick_colour(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+    %Identicon.Image{image | colour: {red, green, blue}}
   end
 
   def hash_input(input) do
